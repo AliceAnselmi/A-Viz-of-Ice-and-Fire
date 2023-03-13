8e2b33d84f892e314cecfc273b6710a2909c2639
Alice Anselmi <87475490+AliceAnselmi@users.noreply.github.com>
2023-03-13 19:03:52 +0100

    bugfix

--- script_map.js
+++ script_map.js
@@ -19,6 +19,7 @@ var g;
 const bottombar_width = 1200;
 const bottombar_height = 120;
 const sidebar_height = height - bottombar_height;
+const sidebar_width = 300;
 const sidebar_limit = 500;
 var Ytranslation_rescale = 0;
 
